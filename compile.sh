6810942e7331620682296d6eea7f8794c38d27b8
Bruno Cordeiro <cordeiro.bruno@proton.me>
2026-06-02 19:32:12 -0300

    Getting Started GJS

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@
 
 mkdir -p ~/.local/share/gnome-shell/extensions/volume-mixer@cordeiro-bruno.github.io
 cp -r  * ~/.local/share/gnome-shell/extensions/volume-mixer@cordeiro-bruno.github.io
-journalctl -f -o cat /usr/bin/gnome-shell
-gnome-extensions enable volume-mixer@cordeiro-bruno.github.io
+#gnome-extensions enable volume-mixer@cordeiro-bruno.github.io
+#journalctl -f -o cat /usr/bin/gnome-shell
--- extension.js
+++ extension.js
@@ -1,22 +1,28 @@
-'use strict';
+import St from 'gi://St';
 
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-var VolumeMixer = imports.modules.VolumeMixer;
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 
-function init() {
-    log("init ${Me.metadata.name} ${Me.metadata.version}")
-}
 
-function enable() {
-    log("enable ${Me.metadata.name} ${Me.metadata.version}")
-    VolumeMixer = new VolumeMixer();
-}
+export default class ExampleExtension extends Extension {
+    enable() {
+        // Create a panel button
+        this._indicator = new PanelMenu.Button(0.0, this.metadata.name, false);
 
-function disable() {
-    log("disable ${Me.metadata.name} ${Me.metadata.version}")
-    if (VolumeMixer !== null) {
-        VolumeMixer.destroy();
-        VolumeMixer = null;
+        // Add an icon
+        const icon = new St.Icon({
+            icon_name: 'face-laugh-symbolic',
+            style_class: 'system-status-icon',
+        });
+        this._indicator.add_child(icon);
+
+        // Add the indicator to the panel
+        Main.panel.addToStatusArea(this.uuid, this._indicator);
+    }
+
+    disable() {
+        this._indicator?.destroy();
+        this._indicator = null;
     }
 }
